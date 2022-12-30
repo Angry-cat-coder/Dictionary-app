c166cd1b550d098d255cab0bdd16283fd1a1818c
Angry-cat-coder <y.v.isaenko@gmail.com>
2022-12-31 01:49:30 +0200

    Added css style

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello from AngryKitty</p>
+        <p>Welcome to AngryKitty's dictionary!</p>
       </header>
     </div>
   );
