c728ab2e12f2bc0d129e57d5eeaf436160d1c095
Angry-cat-coder <y.v.isaenko@gmail.com>
2023-01-07 20:41:09 +0200

    added axios integration

--- src/Dictionarysearch.js
+++ src/Dictionarysearch.js
@@ -1,20 +1,27 @@
 import React, { useState } from "react";
 import "./Dictionarysearch.css";
+import axios from "axios";
 export default function Dictionarysearch() {
   let [keyWord, setkeyWord] = useState(" ");
   function search(event) {
     event.preventDefault();
     alert(`Sarching for ${keyWord} definition`);
+    let upiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyWord}`;
+    axios.get(upiUrl).then(showResponse);
   }
+  //https://dictionaryapi.dev/
   function showKeyword(event) {
     setkeyWord(event.target.value);
+  }
+  function showResponse(response) {
+    console.log(response.data);
   }
   return (
     <div className="Dictionarysearch">
       "Hello"{" "}
       <form onSubmit={search}>
         {" "}
-        <input type={search} onChange={showKeyword} />
+        <input type="search" onChange={showKeyword} />
       </form>
     </div>
   );
